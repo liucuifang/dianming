25cf7c1114388c65956470e754d41e2d9c82bd18
liucuifang <1459282112@qq.com>
2016-11-11 21:45:21 +0800

    上传点名app项目文件

---
+++ ListView_test/app/src/main/java/com/example/guowh/listview_test/info.java
@@ -0,0 +1,44 @@
+package com.example.guowh.listview_test;
+
+/**
+ * Created by guowh on 2016/10/28.
+ */
+
+public class info {
+    private String name;
+    private String stu_id;
+    private int state;
+    private int photo;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setStudent_id(String stu_id) {
+        this.stu_id = stu_id;
+    }
+
+    public String getStudent_id() {
+        return stu_id;
+    }
+
+    public void setState(int state) {
+        this.state = state;
+    }
+
+    public int getState() {
+        return state;
+    }
+
+    public void setPhoto(int photo) {
+        this.photo = photo;
+    }
+
+    public int getPhoto() {
+        return photo;
+    }
+}
